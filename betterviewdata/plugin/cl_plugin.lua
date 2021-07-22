316caa24dbd26402caffd9f798d119dec908405e
bubsybubz <86645992+bubsybubz@users.noreply.github.com>
2021-07-21 21:06:43 -0400

    Update cl_plugin.lua

--- betterviewdata/plugin/cl_plugin.lua
+++ betterviewdata/plugin/cl_plugin.lua
@@ -1,14 +1,13 @@
 local PLUGIN = PLUGIN;
-local Clockwork = Clockwork;
 
-Clockwork.datastream:Hook("EditData", function(data)
+netstream:Hook("EditData", function(1, 2, 3, 4)
 	if (IsValid( data[1] )) then
 		if (Schema.dataPanel and Schema.dataPanel:IsValid()) then
 			Schema.dataPanel:Close();
 			Schema.dataPanel:Remove();
 		end;
 
-		Schema.dataPanel = vgui.Create("cwData");
+		Schema.dataPanel = vgui.Create("ixData");
 		Schema.dataPanel:Populate(data[1], data[2] or "", data[3] or {}, data[4] or "");
 		Schema.dataPanel:MakePopup();
 
